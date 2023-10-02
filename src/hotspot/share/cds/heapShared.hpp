86bafeec8a75c7590069d06051308f55c80cd43d
Jiangli Zhou <jianglizhou@google.com>
2023-10-02 11:14:18 -0700

    Remove runtime archived heap oopmap check (non-product only code).
    The code was only enabled for non-product binary in ArchiveHeapLoader::patch_embedded_pointers. ArchiveHeapLoader::patch_embedded_pointers is called during MetaspaceShared::initialize_shared_spaces to patch all archived Java heap pointers when runtime relocation occurs (e.g. archived Java heap regions cannot be mmap at the desired addresses due to runtime Java heap size difference). That's done early during VM initialization and before SystemDictionary::resolve_well_known_classes. The calculate_oopmap operations may access some of the well-known klasses during oop iteration. That could cause crashes since the well-known klasses are not loaded/resolved at the time.
    
    When loading/resolving a shared well-known klass, SystemDictionary::resolve_wk_klass loads and restores the archived klass and mirror object. So it's not feasible to move HeapShared::patch_archived_heap_embedded_pointers to a later point after resolving some of the needed well-known classes during VM initialization. Hence removing the runtime sanity check.

--- src/hotspot/share/cds/heapShared.hpp
+++ src/hotspot/share/cds/heapShared.hpp
@@ -362,7 +362,6 @@ private:
                                              KlassSubGraphInfo* subgraph_info,
                                              oop orig_obj);
 
-  static ResourceBitMap calculate_oopmap(MemRegion region); // marks all the oop pointers
   static void add_to_dumped_interned_strings(oop string);
 
   // Scratch objects for archiving Klass::java_mirror()
