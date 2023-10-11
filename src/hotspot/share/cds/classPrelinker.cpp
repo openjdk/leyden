77d438dd61c25fd1fc2981f2955c7b94b04d54d8
iklam <ilam3388@gmail.com>
2023-10-10 20:05:59 -0700

    Fixed crash when perf counters were used without UsePerfData

--- src/hotspot/share/cds/classPrelinker.cpp
+++ src/hotspot/share/cds/classPrelinker.cpp
@@ -1133,7 +1133,9 @@ void ClassPrelinker::jvmti_agent_error(InstanceKlass* expected, InstanceKlass* a
 
 void ClassPrelinker::runtime_preload(PreloadedKlasses* table, Handle loader, TRAPS) {
   elapsedTimer timer;
-  timer.start();
+  if (UsePerfData) {
+    timer.start();
+  }
   Array<InstanceKlass*>* preloaded_klasses;
   Array<InstanceKlass*>* initiated_klasses = nullptr;
   const char* loader_name;
@@ -1219,8 +1221,10 @@ void ClassPrelinker::runtime_preload(PreloadedKlasses* table, Handle loader, TRA
     }
   }
 
-  timer.stop();
-  _perf_class_preload_time->inc(timer.ticks());
+  if (UsePerfData) {
+    timer.stop();
+    _perf_class_preload_time->inc(timer.ticks());
+  }
 
 #if 0
   // Hmm, does JavacBench crash if this block is enabled??
