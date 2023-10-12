6be1da15c4a5ae0ba5bc3aa0dc981a19a442a9d6
Vladimir Ivanov <vladimir.x.ivanov@oracle.com>
2023-10-11 20:21:21 -0700

    Improve logging

--- src/hotspot/share/interpreter/linkResolver.cpp
+++ src/hotspot/share/interpreter/linkResolver.cpp
@@ -1746,14 +1746,14 @@ void LinkResolver::resolve_invokehandle(CallInfo& result, const constantPoolHand
                             THREAD->class_being_initialized() == nullptr);
 
   LinkInfo link_info(pool, index, Bytecodes::_invokehandle, CHECK);
+  { // Check if the call site has been bound already, and short circuit:
+    bool is_done = resolve_previously_linked_invokehandle(result, link_info, pool, index, CHECK);
+    if (is_done) return;
+  }
   if (log_is_enabled(Info, methodhandles)) {
     ResourceMark rm(THREAD);
     log_info(methodhandles)("resolve_invokehandle %s %s", link_info.name()->as_C_string(),
                             link_info.signature()->as_C_string());
-  }
-  { // Check if the call site has been bound already, and short circuit:
-    bool is_done = resolve_previously_linked_invokehandle(result, link_info, pool, index, CHECK);
-    if (is_done) return;
   }
   resolve_handle_call(result, link_info, CHECK);
 }
