bd212673822a21164fbf57b255005339d28ef509
Ioi Lam <iklam@openjdk.org>
2025-04-01 20:52:07 -0700

    Add resolved class/field/method in FinalImagesRecipe; add missing lock

--- src/hotspot/share/runtime/mutexLocker.hpp
+++ src/hotspot/share/runtime/mutexLocker.hpp
@@ -139,6 +139,7 @@ extern Mutex*   UnregisteredClassesTable_lock;   // UnregisteredClassesTableTabl
 extern Mutex*   LambdaFormInvokers_lock;         // Protecting LambdaFormInvokers::_lambdaform_lines
 extern Mutex*   ScratchObjects_lock;             // Protecting _scratch_xxx_table in heapShared.cpp
 extern Mutex*   ArchivedObjectTables_lock;       // Protecting the table used by HeapShared::get_archived_object_permanent_index()
+extern Mutex*   FinalImageRecipes_lock;          // Protecting the tables used by FinalImageRecipes.
 #endif // INCLUDE_CDS
 #if INCLUDE_JFR
 extern Mutex*   JfrStacktrace_lock;              // used to guard access to the JFR stacktrace table
