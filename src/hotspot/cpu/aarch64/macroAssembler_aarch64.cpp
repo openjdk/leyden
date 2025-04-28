594d2ed7fe4497256d3aab651daae5df88dc0b1f
Vladimir Kozlov <vladimir.kozlov@oracle.com>
2025-04-27 19:38:08 -0700

    Fixed merge

--- src/hotspot/cpu/aarch64/macroAssembler_aarch64.cpp
+++ src/hotspot/cpu/aarch64/macroAssembler_aarch64.cpp
@@ -3261,12 +3261,13 @@ void MacroAssembler::resolve_global_jobject(Register value, Register tmp1, Regis
 }
 
 void MacroAssembler::stop(const char* msg) {
-  BLOCK_COMMENT(msg);
+  // Skip AOT caching C strings in scratch buffer.
+  const char* str = (code_section()->scratch_emit()) ? msg : AOTCodeCache::add_C_string(msg);
+  BLOCK_COMMENT(str);
   // load msg into r0 so we can access it from the signal handler
   // ExternalAddress enables saving and restoring via the code cache
-  lea(c_rarg0, ExternalAddress((address) msg));
+  lea(c_rarg0, ExternalAddress((address) str));
   dcps1(0xdeae);
-  AOTCodeCache::add_C_string(msg);
 }
 
 void MacroAssembler::unimplemented(const char* what) {
--- src/hotspot/cpu/x86/macroAssembler_x86.cpp
+++ src/hotspot/cpu/x86/macroAssembler_x86.cpp
@@ -367,11 +367,12 @@ void MacroAssembler::stop(const char* msg) {
     lea(c_rarg1, InternalAddress(rip));
     movq(c_rarg2, rsp); // pass pointer to regs array
   }
-  lea(c_rarg0, ExternalAddress((address) msg));
+  // Skip AOT caching C strings in scratch buffer.
+  const char* str = (code_section()->scratch_emit()) ? msg : AOTCodeCache::add_C_string(msg);
+  lea(c_rarg0, ExternalAddress((address) str));
   andq(rsp, -16); // align stack as required by ABI
   call(RuntimeAddress(CAST_FROM_FN_PTR(address, MacroAssembler::debug64)));
   hlt();
-  AOTCodeCache::add_C_string(msg);
 }
 
 void MacroAssembler::warn(const char* msg) {
