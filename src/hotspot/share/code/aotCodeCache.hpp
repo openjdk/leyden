446ac35fd97b893022d22789f8613b438f12960a
Ashutosh Mehra <asmehra@redhat.com>
2025-06-07 22:20:01 -0400

    Fix minimal compile failure
    Signed-off-by: Ashutosh Mehra <asmehra@redhat.com>

--- src/hotspot/share/code/aotCodeCache.hpp
+++ src/hotspot/share/code/aotCodeCache.hpp
@@ -713,8 +713,8 @@ public:
 
   void fix_relocations(CodeBlob* code_blob, GrowableArray<Handle>* oop_list = nullptr, GrowableArray<Metadata*>* metadata_list = nullptr) NOT_CDS_RETURN;
 #ifndef PRODUCT
-  void read_asm_remarks(AsmRemarks& asm_remarks, bool use_string_table);
-  void read_dbg_strings(DbgStrings& dbg_strings, bool use_string_table);
+  void read_asm_remarks(AsmRemarks& asm_remarks, bool use_string_table) NOT_CDS_RETURN;
+  void read_dbg_strings(DbgStrings& dbg_strings, bool use_string_table) NOT_CDS_RETURN;
 #endif // PRODUCT
 
   void print_on(outputStream* st);
