a4dc4e3961b87c5c5f0abdbb8858272bd434da72
Ashutosh Mehra <asmehra@redhat.com>
2025-08-14 17:38:44 -0400

    Fix compile failure
    Signed-off-by: Ashutosh Mehra <asmehra@redhat.com>

--- src/hotspot/share/cds/aotCacheAccess.cpp
+++ src/hotspot/share/cds/aotCacheAccess.cpp
@@ -67,6 +67,12 @@ uint AOTCacheAccess::delta_from_base_address(address addr) {
   return (uint)pointer_delta(requested_addr, (address)MetaspaceShared::requested_base_address(), 1);
 }
 
+uint AOTCacheAccess::convert_method_to_offset(Method* method) {
+  assert(MetaspaceShared::is_in_shared_metaspace(method), "method %p is not in AOTCache", method);
+  uint offset = (uint)pointer_delta((address)method, (address)SharedBaseAddress, 1);
+  return offset;
+}
+
 #if INCLUDE_CDS_JAVA_HEAP
 int AOTCacheAccess::get_archived_object_permanent_index(oop obj) {
   return HeapShared::get_archived_object_permanent_index(obj);
