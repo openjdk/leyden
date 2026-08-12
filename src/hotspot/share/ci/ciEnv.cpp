eff0d9b19ee6269f4ba811e01d0ae3de26f888d1
Vladimir Kozlov <vladimir.kozlov@oracle.com>
2026-08-12 15:03:31 -0700

    Replace one AOT code with other when TieredCompilation is off

--- src/hotspot/share/ci/ciEnv.cpp
+++ src/hotspot/share/ci/ciEnv.cpp
@@ -1046,7 +1046,7 @@ void ciEnv::make_code_usable(JavaThread* thread, ciMethod* target, bool preload,
   methodHandle method(thread, target->get_Method());
 
   if (entry_bci == InvocationEntryBci) {
-    if (TieredCompilation) {
+    if (TieredCompilation || is_aot_compile()) {
       // If there is an old version we're done with it
       nmethod* old = method->code();
       if (TraceMethodReplacement && old != nullptr) {
